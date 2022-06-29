34ac93c79e90cde82d7ac7655c10df1905a98f15
DanielBerkhout <75730299+DanielBerkhout@users.noreply.github.com>
2022-06-29 22:08:04 +0200

    Add files via upload
    more movement functions

--- Applicatie/robot.cpp
+++ Applicatie/robot.cpp
@@ -26,12 +26,85 @@ robot::robot(uint8_t F_R_Yaw_adress, uint8_t F_R_Pitch_adress,
     R_L_Pitch(R_L_Pitch_adress)    
     {}
 
+
+void robot::F_R_Voor(){
+    F_R_Pitch.write(150); //omhoog
+    hwlib::wait_ms(wait_ms);
+    F_R_Yaw.write(150); //naar voren
+    hwlib::wait_ms(wait_ms);
+    F_R_Pitch.write(180); //omlaag
+    hwlib::wait_ms(wait_ms);
+}
+
+void robot::R_R_Voor(){
+    R_R_Pitch.write(30); //omhoog
+    hwlib::wait_ms(wait_ms);
+    R_R_Yaw.write(150); //naar voren
+    hwlib::wait_ms(wait_ms);
+    R_R_Pitch.write(0); //omlaag
+    hwlib::wait_ms(wait_ms);
+}
+
+void robot::F_L_Voor(){
+    F_L_Pitch.write(50);
+    hwlib::wait_ms(wait_ms);
+    F_L_Yaw.write(30);
+    hwlib::wait_ms(wait_ms);
+    F_L_Pitch.write(80);
+    hwlib::wait_ms(wait_ms);
+}
+
+void robot::R_L_Voor(){
+    R_L_Pitch.write(150); //omhoog
+    hwlib::wait_ms(wait_ms);
+    R_L_Yaw.write(50); //naar voren
+    hwlib::wait_ms(wait_ms);
+    R_L_Pitch.write(180); //omlaag
+    hwlib::wait_ms(wait_ms);
+}
+
+void robot::R_R_Achter(){
+    R_R_Pitch.write(30); //omhoog
+    hwlib::wait_ms(wait_ms);
+    R_R_Yaw.write(50); //naar achter
+    hwlib::wait_ms(wait_ms);
+    R_R_Pitch.write(0); //omlaag
+    hwlib::wait_ms(wait_ms);
+}
+
+void robot::F_R_Achter(){
+    F_R_Pitch.write(150); //omhoog
+    hwlib::wait_ms(wait_ms);
+    F_R_Yaw.write(50); //naar achter
+    hwlib::wait_ms(wait_ms);
+    F_R_Pitch.write(180); //omlaag
+    hwlib::wait_ms(wait_ms);
+}
+
+void robot::R_L_Achter(){
+    R_L_Pitch.write(150); //omhoog
+    hwlib::wait_ms(wait_ms);
+    R_L_Yaw.write(130); //naar achter
+    hwlib::wait_ms(wait_ms);
+    R_L_Pitch.write(180); //omlaag
+    hwlib::wait_ms(wait_ms);
+}
+
+void robot::F_L_Achter(){
+    F_L_Pitch.write(50);
+    hwlib::wait_ms(wait_ms);
+    F_L_Yaw.write(130);
+    hwlib::wait_ms(wait_ms);
+    F_L_Pitch.write(80);
+    hwlib::wait_ms(wait_ms);
+}
+
+
 ///\brief
 ///loop naar voren
 ///\details
 ///doet "times" keer de naar voren beweging
 void robot::walk_forward(int times){
-    int wait_ms = 300;
     F_R_Yaw.write(90);  //allemaal recht
     F_L_Yaw.write(90);
     R_R_Yaw.write(90);
@@ -39,35 +112,10 @@ void robot::walk_forward(int times){
     hwlib::wait_ms(wait_ms);
     
     for(int i=0; i<times; i++){
-    F_R_Pitch.write(150); //omhoog
-    hwlib::wait_ms(wait_ms);
-    F_R_Yaw.write(150); //naar voren
-    hwlib::wait_ms(wait_ms);
-    F_R_Pitch.write(180); //omlaag
-    hwlib::wait_ms(wait_ms);
-    
-    R_R_Pitch.write(30); //omhoog
-    hwlib::wait_ms(wait_ms);
-    R_R_Yaw.write(150); //naar voren
-    hwlib::wait_ms(wait_ms);
-    R_R_Pitch.write(0); //omlaag
-    hwlib::wait_ms(wait_ms);
-    
-    F_L_Pitch.write(50);
-    hwlib::wait_ms(wait_ms);
-    F_L_Yaw.write(30);
-    hwlib::wait_ms(wait_ms);
-    F_L_Pitch.write(80);
-    hwlib::wait_ms(wait_ms);
-    
-
-    R_L_Pitch.write(150); //omhoog
-    hwlib::wait_ms(wait_ms);
-    R_L_Yaw.write(50); //naar voren
-    hwlib::wait_ms(wait_ms);
-    R_L_Pitch.write(180); //omlaag
-    hwlib::wait_ms(wait_ms);
-    
+        F_R_Voor();
+        R_R_Voor();
+        F_L_Voor();
+        R_L_Voor();
     
     F_R_Yaw.write(50);
     F_L_Yaw.write(130);
@@ -91,5 +139,80 @@ void robot::walk_forward(int times){
 ///\details
 ///doet "times" keer de naar achter beweging   
 void robot::walk_backward(int times){
+    int wait_ms = 300;
+    F_R_Yaw.write(90);  //allemaal recht
+    F_L_Yaw.write(90);
+    R_R_Yaw.write(90);
+    R_L_Yaw.write(90);
+    hwlib::wait_ms(wait_ms);
     
+    for(int i=0; i<times; i++){
+        R_R_Achter();
+        F_R_Achter();
+        R_L_Achter();
+        F_L_Achter();       
+    
+    F_R_Yaw.write(150);
+    F_L_Yaw.write(30);
+    R_R_Yaw.write(150);
+    R_L_Yaw.write(50);
+    hwlib::wait_ms(wait_ms); //allemaal naar voor 
+    }
+    
+    F_R_Yaw.write(90);  //allemaal recht
+    F_L_Yaw.write(90);
+    R_R_Yaw.write(90);
+    R_L_Yaw.write(90);
+    hwlib::wait_ms(wait_ms);
+    
+}
+
+void robot::turn_left(int times){
+    F_R_Yaw.write(90);  //allemaal recht
+    F_L_Yaw.write(90);
+    R_R_Yaw.write(90);
+    R_L_Yaw.write(90);
+    hwlib::wait_ms(wait_ms);
+    for(int i=0; i<times; i++){
+    F_R_Voor();
+    R_R_Voor();
+    F_L_Achter();
+    R_L_Achter();
+    
+    F_L_Yaw.write(30);
+    R_L_Yaw.write(50);
+    F_R_Yaw.write(50);
+    R_R_Yaw.write(50);
+    }
+    
+    F_R_Yaw.write(90);  //allemaal recht
+    F_L_Yaw.write(90);
+    R_R_Yaw.write(90);
+    R_L_Yaw.write(90);
+    hwlib::wait_ms(wait_ms);
+    
+}
+
+void robot::turn_right(int times){
+    F_R_Yaw.write(90);  //allemaal recht
+    F_L_Yaw.write(90);
+    R_R_Yaw.write(90);
+    R_L_Yaw.write(90);
+    hwlib::wait_ms(wait_ms);
+    for(int i=0; i<times; i++){
+        R_R_Achter();
+        F_R_Achter();
+        R_L_Voor();
+        F_L_Voor();
+        
+    F_L_Yaw.write(130);
+    R_L_Yaw.write(130);
+    F_R_Yaw.write(150);
+    R_R_Yaw.write(150);
+    }
+    F_R_Yaw.write(90);  //allemaal recht
+    F_L_Yaw.write(90);
+    R_R_Yaw.write(90);
+    R_L_Yaw.write(90);
+    hwlib::wait_ms(wait_ms);
 }
--- Applicatie/robot.hpp
+++ Applicatie/robot.hpp
@@ -15,7 +15,18 @@ private:
     servo R_R_Pitch;
     
     servo R_L_Yaw;
-    servo R_L_Pitch;  
+    servo R_L_Pitch;
+    
+    int wait_ms = 100;
+void F_R_Voor();
+void F_R_Achter();
+void F_L_Voor();
+void F_L_Achter();
+void R_R_Voor();
+void R_R_Achter();
+void R_L_Voor();
+void R_L_Achter();
+
 public:
     robot(uint8_t F_R_Yaw, uint8_t F_R_Pitch,
           uint8_t F_L_Yaw, uint8_t F_L_Pitch,
@@ -23,6 +34,8 @@ public:
           uint8_t R_L_Yaw, uint8_t R_L_Pitch);
     void walk_forward(int times);
     void walk_backward(int times);
+    void turn_left(int times);
+    void turn_right(int times);
 
 };
 
